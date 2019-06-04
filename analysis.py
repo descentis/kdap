e009d96e0c447dde91cba6a2d356fef8d20f82d3
descentis <mt4descentis@gmail.com>
2019-06-04 15:47:50 +0530

    added the function find tag to get the relevant tags from stack exchange data dump

--- analysis.py
+++ analysis.py
@@ -1995,3 +1995,118 @@ class knolAnalysis(object):
             processDict[i+1].join()
 
         return RevisionEdits
+    
+    
+    @staticmethod
+    def findTags(list_tags, *args, **kwargs):
+        if(kwargs.get('file_path')!=None):
+            file_name = kwargs['file_path']            
+            tree = ET.parse(file_name)            
+            root = tree.getroot()
+    
+           
+    
+            uList = []
+            for child in root:
+                if('KnowledgeData' in child.tag):
+                    for ch in child:
+                        if('Instance' in ch.tag):
+                            for newch in ch:
+                                if('Contributors' in newch.tag):
+                                    for chi in newch:
+                                        if('OwnerUserId' in chi.tag):
+                                            if(chi.text not in uList):
+                                                uList.append(chi.text)
+            return uList
+            
+        elif(kwargs.get('file_name')!=None):
+            file_name = kwargs['file_name']
+            for f in file_name:
+                tree = ET.parse(f)            
+                root = tree.getroot()
+                postList = []
+                
+                for child in root:
+                    if('KnowledgeData' in child.tag):
+                        for ch in child:
+                            if('Instance' in ch.tag):
+                                for newch in ch:
+                                    if('Body' in newch.tag):
+                                        for txt in newch:
+                                            if('Text' in txt.tag):                                            
+                                                postList.append(txt.text)                            
+                                    
+                                    if('Tags' in newch.tag):
+                                        
+                                        if(list_tags in newch.text):
+                                                continue
+                                        else:
+                                            postList = []
+                                            print(f)
+                                            
+                                            
+    
+    
+                if(kwargs.get('tagPosts')!=None):
+                    kwargs['tagPosts'][f] = postList
+                    #print(kwargs['revisionLength'])
+        else:
+            print("No arguments provided")    
+    
+    def findAllTags(list_tags, *args, **kwargs):
+        #t1 = time.time()
+        if(kwargs.get('file_list')!=None):
+            file_list = kwargs['file_list']
+            
+        elif(kwargs.get('dir_path')!=None):
+            dir_path = kwargs['dir_path']
+            
+            file_list = glob.glob(dir_path+'/*.knolml')
+            
+        fileNum = len(file_list)
+        
+        if(kwargs.get('c_num')!=None):
+            cnum = kwargs['c_num']
+        elif(fileNum<24):
+            cnum = fileNum+1           # Bydefault it is 24
+        else:
+            cnum = 24
+        
+        
+        fileList = []
+        if(fileNum<cnum):
+            for f in file_list:
+                fileList.append([f])
+            
+        else:           
+    
+            f = np.array_split(file_list,cnum)
+            for i in f:
+                fileList.append(i.tolist())        
+        
+        
+    
+        
+        manager = Manager()
+        tagPosts = manager.dict()
+    
+        l = Lock()
+        processDict = {}
+        if(fileNum<cnum):
+            pNum = fileNum
+        else:
+            pNum = cnum
+        for i in range(pNum):
+            processDict[i+1] = Process(target=knolAnalysis.findTags, args=(list_tags), kwargs={'file_name':fileList[i],'tagPosts':tagPosts,'l': l})
+            #processDict[i+1] = Process(target=self.countWords, kwargs={'file_name':fileList[i], 'lastRev':lastRev,'l': l})
+        for i in range(pNum):
+            processDict[i+1].start()
+        
+        for i in range(pNum):
+            processDict[i+1].join()  
+        
+        '''
+        t2 = time.time()
+        print(t2-t1)
+        '''
+        return tagPosts     
