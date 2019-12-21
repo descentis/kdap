269618f728b8bcafde4e16e8796c9e18e4ec6040
descentis <mt4descentis@gmail.com>
2019-12-21 11:01:35 +0530

    testing the download function

--- analysis.py
+++ analysis.py
@@ -390,8 +390,9 @@ class knol(object):
             if kwargs.get('article_list')!=None:
                 article_list = kwargs['article_list']
                 key = 'article_list'
-                articles = self.get_article_name(article_list)
+                #articles = self.get_article_name(article_list)
                 self.download_from_dump(home, article_list, key)
+                
                 if compress_bool:
                     wikiConverter.compressAll(home+'/knolml_dataset/output/', output_dir=destdir)
                 else:
