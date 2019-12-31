0b9829416dddcd926d2874514b6533ef890b2525
descentis <mt4descentis@gmail.com>
2019-12-31 22:11:42 +0530

    added the test cases for wikiExtract

--- tests/test_wikiExtract.py
+++ tests/test_wikiExtract.py
@@ -30,5 +30,26 @@ class TestWikiExtract(unittest.TestCase):
         C = we.get_articles_by_category('Left-wing advocacy groups in France')
         self.assertEqual(C, d)
     
+    
+    def test_template(self):
+        # testing the template method
+        t = {'python': [{'pageid': 45459, 'ns': 0, 'title': 'Control flow'},
+  {'pageid': 59231, 'ns': 0, 'title': 'Exception handling'},
+  {'pageid': 1010628, 'ns': 0, 'title': 'Method overriding'},
+  {'pageid': 23655350,
+   'ns': 4,
+   'title': 'Wikipedia:Bots/Requests for approval/jverlindaBot'},
+  {'pageid': 58886025, 'ns': 2, 'title': 'User:Lpwarner/be bold'}]}
+        we = wikiExtract()
+        C = we.get_articles_by_template('python')
+        self.assertEqual(C,t)
+        
+    def test_author_wiki_edits(self):
+        # testing the wiki author edits
+        e = [{'name': 'Ak47', 'missing': ''}]
+        we = wikiExtract()
+        C = we.get_author_wiki_edits(['ak47'])
+        self.assertEqual(C, e)
+        
 if __name__ == '__main__': 
     unittest.main() 
