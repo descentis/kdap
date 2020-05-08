94156bd2e89baeb39d9bab1905daa743fa9f34b3
descentis <mt4descentis@gmail.com>
2020-05-08 18:57:59 +0530

    added the graph analysis functions in the analysis files

--- kdap/analysis.py
+++ kdap/analysis.py
@@ -29,6 +29,7 @@ from mwviews.api import PageviewsClient
 from kdap.converter.qaConverter import qaConverter
 from kdap.wikiextract.knolml_wikiextractor import QueryExecutor
 from collections import Counter 
+import kdap.wiki_graph.graph_creater as gc
 
 class instances(object):
     
@@ -2939,3 +2940,87 @@ class knol(object):
         print(t2-t1)
         '''
         return tagPosts 
+
+
+    # Graph methods starts here. Please check the graph_creater function for more information
+    def get_graph_by_wikiarticle(self, article_name):
+        '''
+        **Does not require dataset download**
+        
+        This method takes an article name as an argument and creates the induced subgraph
+        among the articles present in there.
+        
+        *Arguments*
+        article_name:
+            Type: String
+            The name of the article for which the interwiki graph has to be created.
+        
+        e.g knol.get_graph_by_name('India')
+        '''
+        gc.get_graph_by_name(article_name)
+        
+        
+    def get_graph_by_wikiarticle_list(self, article_list, *args, **kwargs):
+        '''
+        **Does not require dataset download**
+        
+        This method takes a list of articles name as an argument and creates the induced subgraph
+        among the articles using the wikilinks.
+        
+        *Arguments*
+        article_list:
+            Type: List of strings
+            The list of articles name for which the interwiki graph has to be created.
+        file_name:
+            optional
+            file name by which you want to create the graph
+        eg. knol.get_graph_by_wikiarticle_list(['India', 'Pakistan'], file_name='relation')
+        '''
+        if kwargs.get('file_name')!=None:
+            file_name = kwargs['file_name']+'.graphml'
+            gc.get_inter_graph(article_list, file_name=file_name)
+        else:
+            gc.get_inter_graph(article_list)
+
+
+    def get_graph_by_wikiarticle_countries(self, *args, **kwargs):
+        '''
+        **Does not require dataset download**
+        
+        This method creates the induced subgraph among the articles of all the 
+        wikipedia pages of all the countries using the wikilinks.
+        
+        *Arguments*
+        country_list:
+            optional
+            Type: List of strings
+            The list of countries name for which the interwiki graph has to be created.
+            When not set, the graph is created for all the countries
+        
+        e.g knol.get_graph_by_wikiarticle_countries()
+
+        '''
+        if kwargs.get('country_list')!=None:
+            country_list = kwargs['country_list']
+            gc.all_countries_graph(country_list=country_list)
+        else:
+            gc.all_countries_graph()
+            
+
+    def get_graph_by_wikiarticle_cities(self, country_name):
+        '''
+        **Does not require dataset download**
+        
+        This method creates the induced subgraph among the articles of all the 
+        wikipedia pages of all the cities of a given country using the wikilinks.
+        
+        *Arguments*
+        country_name:
+            Type: string
+            The country name for which the city graph has to be created.
+            
+        
+        e.g knol.get_graph_by_wikiarticle_cities('United States')
+
+        '''
+        gc.get_cities_by_country(country_name)
--- kdap/wiki_graph/graph_creater.py
+++ kdap/wiki_graph/graph_creater.py
@@ -6,6 +6,8 @@ subgraph.
 import wikipedia
 import networkx as nx 
 import pycountry
+import requests
+import mwparserfromhell as mw
 '''
 Following method is used to create the induced subgraph based
 on the list of articles provided.
@@ -58,15 +60,27 @@ def get_graph_by_name(article_name):
         
     nx.write_graphml(G, article_name+'.graphml')
 
-def get_inter_graph(article_list):
+def get_inter_graph(article_list, *args, **kwargs):
+    '''
+    Returns the induced subgraph of wikiarticles provided in the 'article_list'
+    *Arguments*
+    
+    article_list:
+        list of Wikipedia article names e.g ['India', 'United States']
+    file_name:
+        optional
+        name of the file by which you want to save the graph e.g file_name='sample'
+    '''
     G = nx.DiGraph()
     article_name = []
     for article in article_list:
+        article = str(article)
         wiki_names = wikipedia.search(article)
+        #print(wiki_names)
         if article in wiki_names:
             pass
         else:
-            print("The same name article: "+article+" has not been found. Using the name as: "+wiki_names[0])
+            print("The same name article: "+str(article)+" has not been found. Using the name as: "+wiki_names[0])
             article = wiki_names[0]
         G.add_node(article)
         article_name.append(article)
@@ -80,16 +94,22 @@ def get_inter_graph(article_list):
         for link in page_links:
             if link in article_name:
                 G.add_edge(article,link)
-                
-    nx.write_graphml(G, 'cities_graph.graphml')
+    
+    if kwargs.get('file_name')!=None:
+        file_name = kwargs['file_name']+'.graphml'
+    else:
+        file_name = 'sample.graphml'            
+    nx.write_graphml(G, file_name)
 
-def all_countries_graph():
+def all_countries_graph(*args, **kwargs):
+    if kwargs.get('country_list') != None:
+        list_countries = kwargs['country_list']
+    else:
+        list_countries = []
+        for c in pycountry.countries:
+            list_countries.append(str(c.name))
     
-    list_countries = []
-    for c in pycountry.countries:
-        list_countries.append(str(c.name))
-    
-    get_inter_graph(list_countries)
+    get_inter_graph(list_countries, file_name='countries_graph')
     
 
 def cities_by_country_graph():
@@ -102,5 +122,46 @@ def cities_by_country_graph():
     
     get_inter_graph(list_cities)
 
-
+def get_cities_by_country(country_name):
+    '''
+    Creates a graph of city wikiarticles in the provided country name
+    *Arguments*
+    
+    country_name:
+        Name of the country for which you want to create the graph e.g "India"
+    '''
+    article_name = 'List_of_cities_in_'+country_name+'_by_population'
+    wiki_names = wikipedia.search(article_name)
+    #print(wiki_names)
+    if article_name in wiki_names:
+        pass
+    else:
+        print("The same name article: "+str(article_name)+" has not been found. Using the name as: "+wiki_names[0])
+        article = wiki_names[0]
+    url = 'https://en.wikipedia.org/w/api.php?action=parse&prop=wikitext&format=json&page='+article 
+    r = requests.get(url)
+    data = r.json()
+    wikiText = data['parse']['wikitext']['*']
+    iterate = False
+    cities_list = []
+    for line in wikiText.splitlines():
+        #with open('united.txt','a') as myFile:
+        #    myFile.write(line+'\n')
+        
+        if 'wikitable sortable' in line:
+            #print('Yes')
+            iterate=True
+        if iterate:
+            #print(line)
+            p = mw.parse(line)
+            try:
+                cities_list.append(p.filter_wikilinks()[0].title)
+            except:
+                pass
+        if iterate and '|}' in line:
+            break
+        
+    
+    #print(cities_list)
+    get_inter_graph(cities_list, file_name=country_name)
 
