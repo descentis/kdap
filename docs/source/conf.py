46f056d3286445f613192445e77e5133efac8815
descentis <mt4descentis@gmail.com>
2020-08-12 21:59:04 +0530

    added the index path in document conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,6 +18,7 @@ import os
 
 sys.path.insert(0, os.path.abspath('../../'))
 
+master_doc = 'index'
 # -- Project information -----------------------------------------------------
 
 project = 'kdap'
